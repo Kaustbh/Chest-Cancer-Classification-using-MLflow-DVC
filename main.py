b8d713852c7d95b4a827eefae05ece8e79fe92dc
Kaustubh Desale <desalekaustubh3@gmail.com>
2024-04-30 09:11:48 +0530

    Data Ingestion Completed

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from Chest_Cancer_Classifier import logger
+from Chest_Cancer_Classifier.pipline.stage_01_data_ingestion import DataIngestionTrainingPipline
  
 
-logger.info("Welcome to the CnnClassfier")
+STAGE_NAME = 'Data Ingestion stage'
+
+try:
+        logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipline()
+        obj.main()
+        logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<< \n\n")
+     
+except Exception as e:
+         logger.exception(e)
+         raise e
---
+++ src/Chest_Cancer_Classifier/pipline/stage_01_data_ingestion.py
@@ -0,0 +1,33 @@
+from Chest_Cancer_Classifier.config.configuration import ConfigurationManager
+from Chest_Cancer_Classifier.components.data_ingestion import DataIngestion
+from Chest_Cancer_Classifier import logger
+
+STAGE_NAME = 'Data Ingestion stage'
+
+class DataIngestionTrainingPipline:
+
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+if __name__=='__main__':
+     try:
+        logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipline()
+        obj.main()
+        logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<< \n\n")
+     
+     except Exception as e:
+         logger.exception(e)
+         raise e
+
+
+
+        
